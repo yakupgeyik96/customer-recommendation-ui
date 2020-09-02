5fa08c23ed1bcbc620635f59377112c8803cb418
yakupgeyik96 <yakupgeyik537@gmail.com>
2020-09-02 21:49:37 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import  {Redirect, BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./navbar/Navbar";
 import Slider from "./components/Slider";
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/profil" exact component={Slider} />
-          <Route path="/firsatlar" />
-          <Route path="/hakkimizda" />
-        </Switch>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/profil" /> {/* ilk girişte profil sayfasına yönlendirme yap */}
+            </Route>
+            <Route path="/profil" exact component={Slider} /> {/* Profil sayfasına yönlendir */}
+            <Route path="/firsatlar" /> {/* Fırsatlar sayfasına yönlendir */}
+          </Switch>
+        </BrowserRouter>
     );
   }
 }
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
+
 import * as types from './types';
 import users from '../api/users';
 
 export const fetchUser = (id) => async dispatch => {
     const response = await users.get(`/users/${id}`);
-    console.log(response.data);
     dispatch({ type: types.FETCH_USER, payload: response.data });
 };

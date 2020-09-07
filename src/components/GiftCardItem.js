0aa81d5065e11151cf59d1b92050a9f3ff0c7ad1
yakupgeyik96 <yakupgeyik537@gmail.com>
2020-09-07 23:31:55 +0000

    hediye kartları düzenlendi

---
+++ src/components/GiftCardItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../css/GiftCardItem.css';
+import image from '../images/bg1.jpg';
+
+class GiftCardItem extends React.Component {
+    render() {
+        return (
+            <div className="gift-card">
+                <img src={image} alt="gift-card" style={{ width: '100%' }} />
+                <div className="gift-card-content">
+                    <span>{this.props.content}</span>
+                </div>
+                <hr />
+                <div className="gift-card-link">
+                    <a href={this.props.link}>{this.props.linkText}</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default GiftCardItem;

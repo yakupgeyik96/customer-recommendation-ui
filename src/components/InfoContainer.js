d20cb087a2a7fbb05f590f34c050dbe18d3d7478
cerenbattal <cerenbattal97@gmail.com>
2020-09-08 23:27:16 +0300

    design improvements on body components

--- src/components/InfoContainer.js
+++ src/components/InfoContainer.js
@@ -1,13 +1,17 @@
 import React from "react";
 import InfoItem from "./InfoItem";
+import point from "../images/point_icon.PNG";
+import sun from "../images/sun_icon.PNG";
+import ticket from "../images/ticket_image.PNG";
 
 class InfoContainer extends React.Component {
   iconChangeForSeason = (props) => {
-    const season = props.mostFlightSeason;
+    const season = props.mostFlightSeason[0];
+    console.log(season);
     switch (season) {
       case "Yaz":
         return {
-          icon: "sun icon",
+          icon: sun,
           iconColor: "orange",
         };
       case "Sonbahar":
@@ -38,7 +42,7 @@ class InfoContainer extends React.Component {
             beginText={"Bizden bu zamana kadar tam"}
             endText={"bilet aldın!"}
             textSelector={this.props.amountOfTicket}
-            icon={"ticket alternate icon"}
+            icon={ticket}
             iconColor={"#bd9d5a"}
             linkText={"Bir sonrakini almaya ne dersin?"}
             link={"https://www.enuygun.com/ucak-bileti/gezgin/"}
@@ -48,20 +52,20 @@ class InfoContainer extends React.Component {
             endText={
               "insanısın! Seyahatlerini çoğunlukla bu mevsimde yapıyorsun."
             }
-            textSelector={this.props.mostFlightSeason}
+            textSelector={this.props.mostFlightSeason[0]}
             icon={iconSeason.icon}
             iconColor={iconSeason.iconColor}
             linkText={`${this.props.mostFlightSeason} mevsiminin en çok tercih edilen yerlerine göz at!`}
-            link={"https://www.enuygun.com/ucak-bileti/gezgin/"}
+            link={"https://www.enuygun.com/galeri/kategori/turizm-tatil/"}
           />
           <InfoItem
             beginText={"Seninle birlikte tam"}
             endText={"farklı şehir gördük!"}
             textSelector={this.props.numberOfDifferentCities}
-            icon={"map marker icon"}
+            icon={point}
             iconColor={"red"}
             linkText={"Yeni yerler keşfetme zamanı!"}
-            link={"https://www.enuygun.com/ucak-bileti/gezgin/"}
+            link={"https://www.enuygun.com/bilgi/sehir-rehberi/"}
           />
         </div>
       </div>

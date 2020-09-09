179b0103b4ec5f86f4e989ad09443bbd78d0dbb0
yakupgeyik96 <yakupgeyik537@gmail.com>
2020-09-09 16:03:24 +0000

    gerekli tüm api url sorguları eklendi, ilgili action ve reducerlar oluşturuldu

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -8,6 +8,21 @@ export default (state = {}, action) => {
         case types.FETCH_RECOMMENDATIONS:
             return {...state, "recommendations": action.payload };
 
+        case types.FETCH_USER_INFOS:
+            return {...state, "userInfos": action.payload};
+
+        case types.FETCH_TRANSACTIONSCOUNT:
+            return {...state, "transactionscount": action.payload};
+
+        case types.FETCH_LASTFLIGHT:
+            return {...state, "lastflight": action.payload};
+
+        case types.FETCH_DIFFERENT_DESTINATIONS_COUNT:
+            return {...state, "differentdestinationcount": action.payload};
+
+        case types.FETCH_SEASONS:
+            return {...state, "seasons": action.payload};
+
         case types.SPREAD_ID:
             return {...state, "id": action.payload};
 
